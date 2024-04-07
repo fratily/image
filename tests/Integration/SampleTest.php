9b43576d4ce1bbd6178345ebfa8bc9826149ffa1
kento-oka <kento-oka@kentoka.com>
2024-04-07 16:43:19 +0900

    initial commit

---
+++ tests/Integration/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fratily\Tests\ImageGenerator\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function test_sample(): void
+    {
+        self::assertTrue(true); //@phpstan-ignore-line
+    }
+}
